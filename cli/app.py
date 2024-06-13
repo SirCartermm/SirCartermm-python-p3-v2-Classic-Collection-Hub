56a448e8a8dbcd122691fe02f9d8c59b4eb84ab5
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-13 11:38:34 +0300

    modified app.py

--- cli/app.py
+++ cli/app.py
@@ -7,3 +7,26 @@ engine = create_engine('sqlite:///supercars.db')
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def list_supercars():
+    session = Session()
+    supercars = session.query(Supercar).all()
+    for supercar in supercars:
+        click.echo(supercar.name)
+
+@cli.command()
+@click.argument('supercar_id', type=int)
+def like_supercar(supercar_id):
+    session = Session()
+    vote = Vote(supercar_id=supercar_id, vote_type='like')
+    session.add(vote)
+    session.commit()
+
+@cli.command()
+@click.argument('supercar_id', type=int)
+@click.argument('comment_text', type=str)
