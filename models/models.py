478b720465f3fbfad8080317565be378e122431d
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-13 11:47:46 +0300

    modified app.py

--- models/models.py
+++ models/models.py
@@ -1,1 +1,8 @@
 #models.py
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
