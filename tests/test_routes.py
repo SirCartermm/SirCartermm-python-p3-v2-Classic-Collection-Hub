d92024276d70c0298cbc75bfddf7cb3c248c6a3d
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-13 12:05:19 +0300

    modified test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,1 +1,9 @@
 #test_routes.py
+import pytest
+from app import app, db
+from models import User, Supercar, Vote, Comment
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
