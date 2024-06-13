bdc197ed10f746797df9620ec8e8ed14d43a36a2
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-13 12:37:53 +0300

    modified  cli  app.py

--- cli/app.py
+++ cli/app.py
@@ -1,7 +1,8 @@
 #app.py
 import click
 from sqlalchemy.orm import sessionmaker
-from models.supercar import Base, Supercar, User, Vote, Comment
+from sqlalchemy import create_engine
+from models.supercar import Base, Supercar, Vote, Comment
 
 engine = create_engine('sqlite:///supercars.db')
 Base.metadata.create_all(engine)
@@ -10,32 +11,45 @@ Session = sessionmaker(bind=engine)
 
 @click.group()
 def cli():
+    """Supercar CLI"""
     pass
 
 @cli.command()
 def list_supercars():
+    """List all supercars"""
     session = Session()
     supercars = session.query(Supercar).all()
     for supercar in supercars:
-        click.echo(supercar.name)
+        click.echo(f"{supercar.name} ({supercar.description})")
 
 @cli.command()
 @click.argument('supercar_id', type=int)
 def like_supercar(supercar_id):
+    """Like a supercar"""
     session = Session()
-    vote = Vote(supercar_id=supercar_id, vote_type='like')
-    session.add(vote)
-    session.commit()
+    supercar = session.query(Supercar).get(supercar_id)
+    if supercar:
+        vote = Vote(supercar_id=supercar_id, vote_type='like')
+        session.add(vote)
+        session.commit()
+        click.echo(f"Liked supercar {supercar_id} successfully!")
+    else:
+        click.echo("Supercar not found!")
 
 @cli.command()
 @click.argument('supercar_id', type=int)
 @click.argument('comment_text', type=str)
-
 def comment_supercar(supercar_id, comment_text):
+    """Comment on a supercar"""
     session = Session()
-    comment = Comment(supercar_id=supercar_id, comment_text=comment_text)
-    session.add(comment)
-    session.commit()
+    supercar = session.query(Supercar).get(supercar_id)
+    if supercar:
+        comment = Comment(supercar_id=supercar_id, comment_text=comment_text)
+        session.add(comment)
+        session.commit()
+        click.echo(f"Commented on supercar {supercar_id} successfully!")
+    else:
+        click.echo("Supercar not found!")
 
 if __name__ == '__main__':
     cli()
