28d8fc305213962e9ff714a0d05c7b8620086496
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-13 12:05:58 +0300

    modified test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,3 +7,14 @@ from models import User, Supercar, Vote, Comment
 def client():
     with app.test_client() as client:
         yield client
+
+
+def test_registration(client):
+    data = {'username': 'testuser', 'email': 'test@example.com', 'password': 'password'}
+    response = client.post('/register', json=data)
+    assert response.status_code == 201
+
+def test_login(client):
+    data = {'username': 'testuser', 'password': 'password'}
+    response = client.post('/login', json=data)
+    assert response.status_code == 200
