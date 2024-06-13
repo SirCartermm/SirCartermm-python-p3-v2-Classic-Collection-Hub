f2d574ee22ea3aabad36e20db0dab3f4570562a2
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-13 11:36:51 +0300

    modified supercar.py

--- cli/app.py
+++ cli/app.py
@@ -1,1 +1,9 @@
 #app.py
+import click
+from sqlalchemy.orm import sessionmaker
+from models.supercar import Base, Supercar, User, Vote, Comment
+
+engine = create_engine('sqlite:///supercars.db')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
