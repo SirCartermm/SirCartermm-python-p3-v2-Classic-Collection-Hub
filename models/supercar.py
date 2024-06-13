d64b098d41b42841fddce0300267aaeb736a7aa1
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-06-13 11:33:53 +0300

    modified supercar.py

--- models/supercar.py
+++ models/supercar.py
@@ -1,1 +1,18 @@
 #supercar.py
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Supercar(Base):
+    __tablename__ = 'supercars'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    email = Column(String)
